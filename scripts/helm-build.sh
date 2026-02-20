046042771edc1ac6f0fd009422f4f73d0a197430
seb <sebastien.genesta@skeepers.io>
2026-02-20 19:45:51 +0100

    package 0.3.0 rebuild

--- scripts/helm-build.sh
+++ scripts/helm-build.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+# Get the chart version from Chart.yaml file
 VERSION=$(yq -r '.version' ../chart/Chart.yaml)
-echo $VERSION
+# Download helm dependencies
 helm dependency update ../chart
+# Package the chart
 helm package ../chart --destination ../packages --version $VERSION
+# Update the index.yaml file in the packages directory
+helm repo index ../packages/ --url https://genseb13011.github.io/activepieces-helm-chart/packages
